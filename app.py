a663de66fc05ce0de6b4e06b401a6616a4ae9f77
edwinkim97 <edwinkim124@gmail.com>
2022-05-09 00:48:12 -0700

    converted from flask app to static app

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    """DOCSTRINGS NEEDED"""
-
-    return render_template('index.html')
